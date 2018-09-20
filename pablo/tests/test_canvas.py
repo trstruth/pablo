e5f5ec2c63a71d61d06e2d40994a9e01412c5ffa
Tristan Struthers <tristan.struthers@mail.mcgill.ca>
2018-09-20 05:41:40 +0000

    Restructure tests

--- pablo/tests/test_canvas.py
+++ pablo/tests/test_canvas.py
@@ -4,30 +4,41 @@ import numpy as np
 
 class CanvasTests(unittest.TestCase):
 
-    def test_init_generated_image(self):
-        c = pablo.Canvas()
-        c.load_target_image_from_file('pablo.png')
-        c.init_generated_image()
+    def test_init(self):
+        c = pablo.Canvas('pablo.png')
+        self.assertIsNotNone(c.target_image)
         self.assertIsNotNone(c.generated_image)
 
-    def test_load_target_image_from_file(self):
-        c = pablo.Canvas()
-        c.load_target_image_from_file('pablo.png')
-        self.assertIsNotNone(c.target_image)
-
     def test_calculate_error_identity(self):
-        c = pablo.Canvas()
+        c = pablo.Canvas('pablo.png')
         c.target_image = np.zeros([300, 300, 0], dtype=int)
         c.generated_image = np.zeros([300, 300, 0], dtype=int)
 
         self.assertEqual(c.calculate_error(), 0)
 
     def test_calculate_error_nonnegative(self):
-        c = pablo.Canvas()
+        c = pablo.Canvas('pablo.png')
         c.target_image = np.empty([300, 300, 0], dtype=int)
         c.generated_image = np.empty([300, 300, 0], dtype=int)
 
         self.assertEqual(c.calculate_error(), 0)
+
+    def test_gym_reset(self):
+        c = pablo.Canvas('pablo.png')
+        initial_observation = c.reset()
+
+        self.assertIsNotNone(c.generated_image)
+        self.assertEqual(c.generated_image.shape, c.target_image.shape)
+        self.assertIsNotNone(initial_observation)
+
+    def test_gym_step(self):
+        pass
+
+    def test_gym_render(self):
+        pass
+
+    def test_gym_close(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
