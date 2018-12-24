f9e6d506fdc9e582ea43c5408c6c63f2304cb7fb
Tristan Struthers <tristan.struthers@mail.mcgill.ca>
2018-12-23 23:23:43 -0800

    Make slight mods to example script

--- example.py
+++ example.py
@@ -1,16 +1,13 @@
 import pablo
 env = pablo.Canvas('pablo.png')
-for i_episode in range(20):
+for i_episode in range(5):
     observation = env.reset()
     info = {}
     for t in range(500):
         print info
         env.render()
         action = env.action_space.sample()
-        try:
-            observation, reward, done, info = env.step(action)
-        except ValueError:
-            print action
+        observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
